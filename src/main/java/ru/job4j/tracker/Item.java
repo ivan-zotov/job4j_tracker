b57e7eb94a18f34970c5e86a0d1986d74beda15d
EvilEvan <2ivanzotov@gmail.com>
2020-10-27 18:14:03 +0300

    2. Перегрузка конструктора [#363963]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -6,6 +6,15 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+    }
+
+    public Item(int id) {
+    }
+
+    public Item(int id, String name) {
+    }
+
     public LocalDateTime getCreated() {
         return LocalDateTime.now();
     }
